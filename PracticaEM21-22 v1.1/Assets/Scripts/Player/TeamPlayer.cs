860b78c4f254e2aa0fb2c44127baaecb3807f2bf
Alvaroolav3D <alvaroolavD2000@gmail.com>
2022-05-21 21:23:51 +0200

    Ajuste equipo 0
    El jugador con id de equipo 0 significara que no pertenece a ningun equipo

--- PracticaEM21-22 v1.1/Assets/Scripts/Player/TeamPlayer.cs
+++ PracticaEM21-22 v1.1/Assets/Scripts/Player/TeamPlayer.cs
@@ -7,8 +7,10 @@ public class TeamPlayer : NetworkBehaviour
 {
     [SerializeField] private SpriteRenderer teamColourRenderer;
     [SerializeField] private Color[] teamColours;
+    //el teamColours[0] queda reservado para un jugador sin equipo
+    //los 4 indices siguientes son los equipos y el jugador cambiara de color
 
-    public NetworkVariable<byte> teamId = new NetworkVariable<byte>(); //para que una nertworkvariable funciones necesito que herede de networkBehabiour
+    private NetworkVariable<byte> teamId = new NetworkVariable<byte>(); //para que una nertworkvariable funciones necesito que herede de networkBehabiour
 
     private void OnEnable()
     {
@@ -23,7 +25,8 @@ public class TeamPlayer : NetworkBehaviour
     [ServerRpc]
     public void SetTeamServerRpc(byte newTeamId) //el motivo de pedir un byte es que es mas eficiente que un int y para decidir sobre los pocos equipos que hay no necesito mas (en este caso)
     {
-        if(newTeamId > 3) { return; } //teamColours.Length - 1
+        print(teamColours.Length);
+        if(newTeamId > teamColours.Length - 1) { return; } //teamColours.Length - 1 = 4
 
         teamId.Value = newTeamId;
     }
